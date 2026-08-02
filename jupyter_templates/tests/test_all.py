45cfb312f3658353573df744c164f8c29f980cb9
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-02 17:52:15 -0400

    Update test_all.py

--- jupyter_templates/tests/test_all.py
+++ jupyter_templates/tests/test_all.py
@@ -1,4 +1,3 @@
-<<<<<<< before updating
 # *****************************************************************************
 #
 # Copyright (c) 2024, the jupyter_templates authors.
@@ -9,10 +8,3 @@
 # for Coverage
 from jupyter_templates import *
 from jupyter_templates.extension import *
-=======
-from jupyter_templates import *
-
-
-def test_all():
-    assert True
->>>>>>> after updating
